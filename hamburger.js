a3993b77ec0e5cf4a7083ad705a6625fe8bc9839
knpt33 <knpt33@mail.missouri.edu>
2017-11-13 19:04:12 -0600

    nav, categories, hamburger

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('#nav').slideToggle(500);
+  });//end slide toggle
+
+//When window is resized, the display:none is turned off for the ul so the menu displays on click or touch.
+  $(window).resize(function() {
+     if (  $(window).width() > 500 ) {
+    $('#nav').removeAttr('style');
+   }
+  });//end resize
+});//end ready
